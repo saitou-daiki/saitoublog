332d25d87f9e90566d6757c749b91c5411b1ffaa
saitou-daiki <alsocorykk@gmail.com>
2019-11-30 15:32:41 +0900

    トップページの画面とJS作成

---
+++ app/assets/javascripts/header.js
@@ -0,0 +1,22 @@
+// var startPos = 0,winScrollTop = 0;
+// $(window).on('scroll',function(){
+//     winScrollTop = $(this).scrollTop();
+//     if (winScrollTop >= startPos) {
+//         $('.site-header').addClass('hide');
+//     } else {
+//         $('.site-header').removeClass('hide');
+//     }
+//     startPos = winScrollTop;
+// });
+
+// $(window).on('scroll',function(){
+//   winScrollTop = $(this).scrollTop();
+//   if (winScrollTop >= startPos) {
+//       if(winScrollTop >= 200){
+//           $('.site-header').addClass('hide');
+//       }
+//   } else {
+//       $('.site-header').removeClass('hide');
+//   }
+//   startPos = winScrollTop;
+// });
