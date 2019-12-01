7bc513772cdaf3842f7b293f8150d082574122c3
saitou-daiki <alsocorykk@gmail.com>
2019-12-01 18:29:13 +0900

    画像とテキスト投稿機能実装

--- README.md
+++ README.md
@@ -30,6 +30,7 @@ Things you may want to cover:
 |email|string|null: false|
 |password|string|null: false|
 |image|text|
+|name|string|
 ### Association
 - has_many :tweets
 - has_many :comments
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,33 @@
 class TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.order('id DESK').limit(20)
+  end
+
+  def new
+    redirect_to action: :index unless user_signed_in?
+    @tweet = Tweet.new
+  end
+  def show
+     @tweet = Tweet.find(params[:tweet_id])
+  
+
+  end
+
+  def create
+    # binding.pry
+    # Tweet.create(name: params[:name], image: params[:image], text: params[:text]) #データを作成し保存する。
+    if Tweet.create(tweet_params)
+      redirect_to root_path
+    else
+      render 'tweets/new'
+    end
+    #カレントユーザーは括弧で囲む必要はない。注意。
+  end
+
+  def tweet_params
+    params.require(:tweet).permit(:image, :content).merge(user_id: current_user.id)
+    #includesメソッド、app.htmlを変えたことで、nameを減らした。アソシエーションで、nicknameがいらなくなり、無くした影響。
+  end
+
+
 end
