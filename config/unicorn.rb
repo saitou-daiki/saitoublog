231d61a39a33799f0c1db3a388a04c455296d4f1
saitou-daiki <saitouabcblue@icloud.com>
2020-09-14 21:52:14 +0900

    unicorn.rb修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,19 +4,20 @@ app_path = File.expand_path('../../../', __FILE__)
 #アプリケーションサーバの性能を決定する
 worker_processes 1
 
-#アプリケーションの設置されているディレクトリを指定
-working_directory "#{app_path}/current"
+#アプリケーションの設置されているディレクトリを指定ー
+# working_directory "#{app_path}/current"
+working_directory app_path
 
-#Unicornの起動に必要なファイルの設置場所を指定
+#Unicornの起動に必要なファイルの設置場所を指定ー
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 
-#ポート番号を指定
+#ポート番号を指定ー
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 
-#エラーのログを記録するファイルを指定
+#エラーのログを記録するファイルを指定ー
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 
-#通常のログを記録するファイルを指定
+#通常のログを記録するファイルを指定ー
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 #Railsアプリケーションの応答を待つ上限時間を設定
